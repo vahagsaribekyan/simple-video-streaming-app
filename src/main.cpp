aeb65979514ef4d9e9a1dbc9f33320b826225678
Vahag <vahag_saribekyab@mail.ru>
2019-05-26 02:43:07 +0400

    try/catch on main function

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 #include "./app/app.h"                                              
+#include <iostream>
 
-int main(int, char**) {                                                          
-    Develandoo::App app;
-    return app.start();                               
+int main(int, char**) {
+    try {
+        std::cout << "Starting the application..." << std::endl;
+        Develandoo::App app;
+        return app.start();
+    } catch(std::exception& e) {
+        std::cout << e.what() << std::endl;
+        return -1;
+    } catch(...) {
+        std::cout << "Something went wrong" << std::endl;
+        return -1;
+    }                             
 }
